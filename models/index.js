e0203c87cfa18379d0ee2e119ff432e5306d84c5
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-08-02 13:39:03 -0400

    refining models

--- models/index.js
+++ models/index.js
@@ -7,18 +7,13 @@ User.hasMany(Post, {
   onDelete: 'CASCADE'
 });
 
+Post.belongsTo(User);
+
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
   onDelete: 'CASCADE'
 });
 
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-})
-
-Post.belongsTo(User);
 Comment.belongsTo(Post);
-Comment.belongsTo(User);
 
 module.exports = { User, Post, Comment };
