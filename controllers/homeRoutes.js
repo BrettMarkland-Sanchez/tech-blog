10eb15ab250517076c376e1b55d0274f5ed999b1
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-08-01 22:35:10 -0400

    handlebars main view changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,8 +5,8 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    // Get all blog posts and JOIN with user data
+    const postData = await Post.findAll({
       include: [
         {
           model: User,
@@ -16,15 +16,14 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
+    res.render('homepage', {
+      posts,
       logged_in: req.session.logged_in
-    
-    });    
-   
+    });
+
   } catch (err) {
     res.status(500).json(err);
   }
