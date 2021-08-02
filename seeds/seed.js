d996b94386aefc1383ffbe5faaacde07db97763c
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-08-01 21:02:59 -0400

    edited seed data to match new models

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,6 +3,7 @@ const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./postData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +13,15 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  for (const post of postData) {
+    await Post.create({
+      ...post
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment
     });
   }
 
